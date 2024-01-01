e278a58b2681a46bcb4fb5127aaf0669af09d8fc
Alexander Plavin <alexander@plav.in>
2024-01-01 00:00:00 +0000

    Aqua tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -56,3 +56,6 @@ add_zoom_patch(plt.gca(), plt.gca(), :horizontal)
 
 import CompatHelperLocal as CHL
 CHL.@check()
+import Aqua
+Aqua.test_all(PyPlotUtils; ambiguities=false, project_toml_formatting=false)
+Aqua.test_ambiguities(PyPlotUtils; recursive=false)
